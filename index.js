b27b70f873a896827796dd304e20c83d5cc9c250
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-01-19 19:23:11 -0600

    learning the basics

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const indexAbout = document.getElementById('index-about');
+
+    indexAbout.addEventListener('click', () => {
+        window.location.href = 'about.html';
+    });
+});
